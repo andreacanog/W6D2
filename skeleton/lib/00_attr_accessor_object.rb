4a2cc09091dd4a8088adbb86dbb6350c42d49289
Andrea Cano <andreacanogisbert@gmail.com>
2022-11-08 16:50:41 -0800

    working on 01-sql-object

---
+++ skeleton/lib/00_attr_accessor_object.rb
@@ -0,0 +1,16 @@
+class AttrAccessorObject
+  def self.my_attr_accessor(*names)
+    names.each do |name|
+      define_method("#{name}".to_sym) do 
+        instance_variable_get("@#{name}")
+      end
+  
+
+      define_method("#{name}=".to_sym) do |new_value|
+        instance_variable_set("@#{name}", new_value)
+      end
+    end
+  end
+end
+
+
---
+++ skeleton/lib/01_sql_object.rb
@@ -0,0 +1,65 @@
+require_relative 'db_connection'
+require 'active_support/inflector'
+# NB: the attr_accessor we wrote in phase 0 is NOT used in the rest
+# of this project. It was only a warm up.
+
+class SQLObject
+  def self.columns
+    return @column if @column 
+   col = DBConnection.execute2(<<-SQL)
+  SELECT
+    *
+  FROM
+    #{table_name}
+  SQL
+    @column = col.first.map(&:to_sym) #{ |c| c.to_sym }
+  end
+
+  def self.finalize!
+
+  end
+
+  def self.table_name=(table_name)
+    @table_name = table_name
+  end
+
+  def self.table_name #intance variable
+    @table_name = self.to_s.downcase + 's'
+  end
+
+  def self.all
+    # ...
+  end
+
+  def self.parse_all(results)
+    # ...
+  end
+
+  def self.find(id)
+    # ...
+  end
+
+  def initialize(params = {})
+    # ...
+  end
+
+  def attributes
+    # ...
+  end
+
+  def attribute_values
+    # ...
+  end
+
+  def insert
+    # ...
+  end
+
+  def update
+    # ...
+  end
+
+  def save
+    # ...
+  end
+end
